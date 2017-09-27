1ac25c43fa2e15ae97d7440c9371e3af84eabeae
Anton Panchenko <nixegend@gmail.com>
2017-09-27 10:08:19 +0300

    sidebar and btn controls > styles

--- src/views/MaterialEditor.vue
+++ src/views/MaterialEditor.vue
@@ -20,30 +20,41 @@ export default {
 
 <template>
   <main-layout>
-    <header slot="controls-header">
+    <header slot="controls-header" class="controls-header">
       <div class="controls-box">
         <custom-button :link="`/${urls.SHADER_EDITOR}`" iconClass="icon-material-editor" />
       </div>
     </header>
 
-    <section slot="controls-content">
+    <section slot="controls-content" class="controls-content">
       controls-content
     </section>
 
-    <footer slot="controls-footer">
+    <footer slot="controls-footer" class="controls-footer">
       <div class="controls-box"> controls box </div>
     </footer>
 
-    <header slot="presentation-header">
+    <header slot="presentation-header" class="presentation-header">
       <div class="controls-box"> presentation-header </div>
     </header>
 
-    <section slot="presentation-content">
-      <aside class="sidebar"> aside </aside>
+    <section slot="presentation-content" class="presentation-content">
+      <aside class="sidebar">
+         <div class="item-obj controls-row">
+           <button type="button" class="show-hide-obj ctrl-btn"><i class="icon-eye" /></button>
+           <button type="button" class="obj-name ctrl-btn">Plane</button>
+           <button type="button" class="trash-bin ctrl-btn"><i class="icon-trash-bin" /></button>
+         </div>
+         <div class="item-obj controls-row">
+           <button type="button" class="show-hide-obj ctrl-btn"><i class="icon-eye-blocked" /></button>
+           <button type="button" class="obj-name ctrl-btn">Sphere</button>
+           <button type="button" class="trash-bin ctrl-btn"><i class="icon-trash-bin" /></button>
+         </div>
+      </aside>
       <div class="canvas-box"> canvas </div>
     </section>
 
-    <footer slot="presentation-footer">
+    <footer slot="presentation-footer" class="presentation-footer">
       <div class="controls-box"> controls box </div>
     </footer>
   </main-layout>
--- src/views/ShaderEditor.vue
+++ src/views/ShaderEditor.vue
@@ -20,30 +20,30 @@ export default {
 
 <template>
   <main-layout>
-    <header slot="controls-header">
+    <header slot="controls-header" class="controls-header">
       <div class="controls-box">
         <custom-button :link="`/${urls.MATERIAL_EDITOR}`" iconClass="icon-shader-editor" />
       </div>
     </header>
 
-    <section slot="controls-content">
+    <section slot="controls-content" class="controls-content">
       controls-content
     </section>
 
-    <footer slot="controls-footer">
+    <footer slot="controls-footer" class="controls-footer">
       <div class="controls-box"> controls box </div>
     </footer>
 
-    <header slot="presentation-header">
+    <header slot="presentation-header" class="presentation-header">
       <div class="controls-box"> presentation-header </div>
     </header>
 
-    <section slot="presentation-content">
-      <aside class="sidebar"> aside </aside>
+    <section slot="presentation-content" class="presentation-content">
+      <aside class="sidebar"> sidebar </aside>
       <div class="canvas-box"> canvas </div>
     </section>
 
-    <footer slot="presentation-footer">
+    <footer slot="presentation-footer" class="presentation-footer">
       <div class="controls-box"> controls box </div>
     </footer>
   </main-layout>

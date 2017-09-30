9ad5d5325681e0e097f4084af10d26149830f851
Anton Panchenko <nixegend@gmail.com>
2017-10-01 01:02:22 +0300

    add "getElementOffsets" utils function

---
+++ src/common/utils/getElementOffsets.js
@@ -0,0 +1,11 @@
+const getElementOffsets = (element) => {
+  const clientRect = element.getBoundingClientRect();
+  const docElelement = document.documentElement;
+
+  return {
+    top: (clientRect.top + window.pageYOffset) - docElelement.clientTop,
+    left: (clientRect.left + window.pageXOffset) - docElelement.clientLeft,
+  };
+};
+
+export default getElementOffsets;

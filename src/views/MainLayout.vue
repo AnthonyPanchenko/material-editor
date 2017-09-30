b1a887c484ba5b1794db3de38963ee68561d6a7c
Anton Panchenko <nixegend@gmail.com>
2017-10-01 01:02:53 +0300

    create and apply "Resize" directive

--- src/views/MainLayout.vue
+++ src/views/MainLayout.vue
@@ -1,6 +1,11 @@
 <script>
+import Resizer from '../directives/Resizer';
+
 export default {
   name: 'MainLayout',
+  directives: {
+    Resizer,
+  },
   props: {
     customClass: {
       type: String,
@@ -16,7 +21,7 @@ export default {
       <slot name="controls-header" />
       <slot name="controls-content" />
       <slot name="controls-footer" />
-      <div class="grab" />
+      <div class="grab" v-resizer="'column'" />
     </section>
     <section class="presentation-layout">
       <slot name="presentation-header" />

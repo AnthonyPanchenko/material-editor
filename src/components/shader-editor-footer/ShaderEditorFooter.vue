404f1352c7f6b1c6051bae0b2eeffb031df42c35
Anton Panchenko <nixegend@gmail.com>
2017-10-12 01:24:07 +0300

    ShaderEditorFooter resize and toggle logic

--- src/components/shader-editor-footer/ShaderEditorFooter.vue
+++ src/components/shader-editor-footer/ShaderEditorFooter.vue
@@ -17,17 +17,15 @@ export default {
 </script>
 
 <template>
-  <resize-box tag="footer" resize="row" :onEndOfResize="onSetFooterHeight" :size="footerHeight" customClass="controls-footer shader-editor-footer">
+  <resize-box tag="footer" resize="row" :onEndOfResize="onSetFooterHeight" :size="footerHeight" customClass="controls-footer shader-editor-footer" :disabled="!isVisibleFooterContent">
     <div class="controls-row">
-      <custom-btn title="Attributes" disabled />
-      <custom-btn title="Uniforms" iconClass="icon-plus" customClass="ctrl-btn-default active" />
-      <custom-btn title="Textures" />
-      <custom-btn iconClass="icon-expand-vertical" customClass="ctrl-btn-default xs" />
+      <custom-btn title="Attributes" :disabled="!isVisibleFooterContent" />
+      <custom-btn title="Uniforms" iconClass="icon-plus" customClass="ctrl-btn-default active" :disabled="!isVisibleFooterContent" />
+      <custom-btn title="Textures" :disabled="!isVisibleFooterContent" />
+      <custom-btn :iconClass="isVisibleFooterContent ? 'icon-collapse-vertical' : 'icon-expand-vertical'" customClass="ctrl-btn-default xs" :onCustomClick="onToggleFooterContent" />
     </div>
-    <transition name="footer-content">
-      <div v-if="isVisibleFooterContent" class="shader-ctrl-settings">
-        footer content
-      </div>
-    </transition>
+    <div v-if="isVisibleFooterContent" class="shader-ctrl-settings">
+      footer content
+    </div>
   </resize-box>
 </template>

7536d7912f8439c9892fd294921a32f0e9daf1d3
Anton Panchenko <nixegend@gmail.com>
2017-10-12 02:08:24 +0300

    add methods to define IconClass and CustomClass

--- src/components/shader-editor-footer/ShaderEditorFooter.vue
+++ src/components/shader-editor-footer/ShaderEditorFooter.vue
@@ -4,24 +4,45 @@ const { mapState, mapActions } = createNamespacedHelpers('shaderEditorFooter');
 
 import ResizeBox from '../resize-box/ResizeBox.vue';
 import CustomBtn from '../../common/components/custom-btn/CustomBtn.vue';
+import tabNames from './constants/tabNames';
 
 export default {
   name: 'ShaderEditorFooter',
+  data() {
+    return {
+      tabNames,
+    }
+  },
   components: {
     CustomBtn,
     ResizeBox,
   },
   computed: mapState(['isVisibleFooterContent', 'activeTabName', 'footerHeight']),
-  methods: mapActions(['onToggleFooterContent', 'onSetActiveTabName', 'onSetFooterHeight']),
+  methods: {
+    ...mapActions(['onToggleFooterContent', 'onSetActiveTabName', 'onSetFooterHeight']),
+    defineIconClass(tabName) {
+      return (this.activeTabName === tabName) && this.isVisibleFooterContent ? 'icon-plus' : '';
+    },
+    defineCustomClass(tabName) {
+      return (this.activeTabName === tabName) && this.isVisibleFooterContent ? 'ctrl-btn-default active' : 'ctrl-btn-default';
+    },
+    onTabClick(event) {
+      if (event.target.dataset.tab === this.activeTabName) {
+        console.log('create new setting');
+      } else {
+        this.onSetActiveTabName(event.target.dataset.tab);
+      }
+    },
+  },
 }
 </script>
 
 <template>
   <resize-box tag="footer" resize="row" :onEndOfResize="onSetFooterHeight" :size="footerHeight" customClass="controls-footer shader-editor-footer" :disabled="!isVisibleFooterContent">
     <div class="controls-row">
-      <custom-btn title="Attributes" :disabled="!isVisibleFooterContent" />
-      <custom-btn title="Uniforms" iconClass="icon-plus" customClass="ctrl-btn-default active" :disabled="!isVisibleFooterContent" />
-      <custom-btn title="Textures" :disabled="!isVisibleFooterContent" />
+      <custom-btn title="Attributes" :iconClass="defineIconClass(tabNames.ATTRIBUTES)" :customClass="defineCustomClass(tabNames.ATTRIBUTES)" :disabled="!isVisibleFooterContent" :data-tab="tabNames.ATTRIBUTES" :onCustomClick="onTabClick" />
+      <custom-btn title="Uniforms" :iconClass="defineIconClass(tabNames.UNIFORMS)" :customClass="defineCustomClass(tabNames.UNIFORMS)" :disabled="!isVisibleFooterContent" :data-tab="tabNames.UNIFORMS" :onCustomClick="onTabClick" />
+      <custom-btn title="Textures" :iconClass="defineIconClass(tabNames.TEXTURES)" :customClass="defineCustomClass(tabNames.TEXTURES)" :disabled="!isVisibleFooterContent" :data-tab="tabNames.TEXTURES" :onCustomClick="onTabClick" />
       <custom-btn :iconClass="isVisibleFooterContent ? 'icon-collapse-vertical' : 'icon-expand-vertical'" customClass="ctrl-btn-default xs" :onCustomClick="onToggleFooterContent" />
     </div>
     <div v-if="isVisibleFooterContent" class="shader-ctrl-settings">

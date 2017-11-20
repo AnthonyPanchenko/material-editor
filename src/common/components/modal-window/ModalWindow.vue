e0c9667ae76bbf26ab3a75ea5f68133201f41f82
Anton Panchenko <nixegend@gmail.com>
2017-11-21 00:55:41 +0200

    ModalWindow component

---
+++ src/common/components/modal-window/ModalWindow.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name: 'ModalWindow',
+  props: {
+    size: {
+      type: String,
+      default: 'md'
+    },
+    isOpen: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      isOpenState: isOpen,
+    };
+  },
+  methods: {
+    onCloseModalWindow() {
+      this.isOpenState = false;
+    }
+  }
+};
+</script>
+
+<template>
+  <transition name="modal-window-fade">
+    <div v-if="isOpenState" class="modal-window" @click="onCloseModalWindow">
+      <div class="container">
+        <slot>Content here</slot>
+      </div>
+    </div>
+  </transition>
+</template>

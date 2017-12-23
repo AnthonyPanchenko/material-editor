a88d9bc34d4d880396ae6b943d31669b196e604a
Anton Panchenko <nixegend@gmail.com>
2017-12-24 01:47:38 +0200

    Vec2Picker

--- src/common/components/vec2-picker/Vec2Picker.vue
+++ src/common/components/vec2-picker/Vec2Picker.vue
@@ -1,14 +1,20 @@
 <script>
 import noop from '../../utils/noop';
 import clamp from '../../utils/clamp';
-import { vectorLength, normalizeVector } from '../../utils/vector';
 import getElementOffsets from '../../utils/getElementOffsets';
 
 export default {
   name: 'Vec2Picker',
   props: {
     name: String,
-    vector: [0, 0],
+    vector: {
+      type: Array,
+      default: () => [0, 0]
+    },
+    size: {
+      type: Number,
+      default: 200
+    },
     onChange: {
       type: Function,
       default: noop
@@ -16,14 +22,11 @@ export default {
   },
   data() {
     return {
-      canvas: null,
-      canvasOffsets: null,
       ctx: null,
-
+      canvas: null,
       isMouseDown: false,
-
-      width: 200,
-      height: 200
+      canvasOffsets: null,
+      halfSize: this.size / 2
     };
   },
   methods: {
@@ -31,18 +34,15 @@ export default {
       this.ctx.strokeStyle = '#000';
       this.ctx.lineWidth = 0.5;
 
-      const halfWidth = 0.5 * this.width;
-      const halfHeight = 0.5 * this.height;
-
       this.ctx.beginPath();
-      this.ctx.moveTo(0, -halfHeight);
-      this.ctx.lineTo(0, halfHeight);
+      this.ctx.moveTo(0, -this.halfSize);
+      this.ctx.lineTo(0, this.halfSize);
       this.ctx.closePath();
       this.ctx.stroke();
 
       this.ctx.beginPath();
-      this.ctx.moveTo(-halfWidth, 0);
-      this.ctx.lineTo(halfWidth, 0);
+      this.ctx.moveTo(-this.halfSize, 0);
+      this.ctx.lineTo(this.halfSize, 0);
       this.ctx.closePath();
       this.ctx.stroke();
     },
@@ -51,29 +51,26 @@ export default {
       this.ctx.font = "9px Arial";
       this.ctx.fillStyle = '#fff';
 
-      this.ctx.fillText("X", 0.5 * this.width - 10, -5);
-      this.ctx.fillText("-X", -0.5 * this.width + 5, 10);
+      this.ctx.fillText("X", this.halfSize - 10, -5);
+      this.ctx.fillText("-X", -this.halfSize + 5, 10);
 
-      this.ctx.fillText("Y", -10, -0.5 * this.height + 10);
-      this.ctx.fillText("-Y", 5, 0.5 * this.height - 5);
+      this.ctx.fillText("Y", -10, -this.halfSize + 10);
+      this.ctx.fillText("-Y", 5, this.halfSize - 5);
     },
 
     drawGrid() {
-      this.ctx.strokeStyle = '#000';
+      const sections = 20;
+      const step = this.size / sections;
+
       this.ctx.beginPath();
+      this.ctx.strokeStyle = '#000';
       this.ctx.lineWidth = 0.2;
 
-      const sections = 20;
-      const step = this.width / sections;
-
-      const startX = -0.5 * this.width;
-      const startY = -0.5 * this.height;
-
       for (let i = 0; i < sections; i++) {
-        this.ctx.moveTo(step * i + startY, startY);
-        this.ctx.lineTo(step * i + startX, this.height);
-        this.ctx.moveTo(startX, step * i + startX);
-        this.ctx.lineTo(this.width, step * i + startX);
+        this.ctx.moveTo(-this.halfSize + step * i, -this.halfSize);
+        this.ctx.lineTo(-this.halfSize + step * i, this.size);
+        this.ctx.moveTo(-this.halfSize, -this.halfSize + step * i);
+        this.ctx.lineTo(this.size, -this.halfSize + step * i);
       }
 
       this.ctx.stroke();
@@ -82,9 +79,10 @@ export default {
     drawPoint(x, y) {
       y *= -1;
 
-      this.ctx.fillStyle = '#fff';
       this.ctx.beginPath();
+      this.ctx.fillStyle = '#fff';
       this.ctx.arc(x, y, 2, 0, 2 * Math.PI, false);
+      this.ctx.closePath();
       this.ctx.fill();
 
       this.ctx.save();
@@ -97,44 +95,31 @@ export default {
       this.ctx.restore();
     },
 
-    onMouseDown(event) {
-      this.isMouseDown = true;
-      this.canvasOffsets = getElementOffsets(this.canvas);
+    draw(event) {
+      this.ctx.clearRect(-this.halfSize, -this.halfSize, this.size, this.size);
 
-      this.ctx.clearRect(-0.5 * this.width, -0.5 * this.height, this.width, this.height);
+      const currentX = clamp(event.pageX - this.canvasOffsets.left, 0, this.size);
+      const currentY = clamp(event.pageY - this.canvasOffsets.top, 0, this.size);
 
-      const x = clamp(event.pageX - this.canvasOffsets.left, 0, this.width);
-      const y = clamp(event.pageY - this.canvasOffsets.top, 0, this.height);
-
-      const pointX = x - (this.width * 0.5);
-      const pointY = -1 * (y - (this.height * 0.5));
+      const x = currentX - this.halfSize;
+      const y = -1 * (currentY - this.halfSize) || 0;
 
       this.drawGrid();
       this.drawAxes();
       this.axesDesignation();
-      this.drawPoint(pointX, pointY);
+      this.drawPoint(x, y);
+      this.onChange(x / this.halfSize, y / this.halfSize);
+    },
+
+    onMouseDown(event) {
+      this.isMouseDown = true;
+      this.canvasOffsets = getElementOffsets(this.canvas);
+      this.draw(event);
     },
 
     onMouseMove(event) {
       if (this.isMouseDown) {
-        this.ctx.clearRect(-0.5 * this.width, -0.5 * this.height, this.width, this.height);
-
-        const x = clamp(event.pageX - this.canvasOffsets.left, 0, this.width);
-        const y = clamp(event.pageY - this.canvasOffsets.top, 0, this.height);
-
-        const pointX = x - (this.width * 0.5);
-        const pointY = -1 * (y - (this.height * 0.5));
-
-        this.drawGrid();
-        this.drawAxes();
-        this.axesDesignation();
-        this.drawPoint(pointX, pointY);
-
-        // const valueX = ((this.range / this.width) * currentX) - (this.range - this.max);
-        // const valueY = ((this.range / this.height) * currentY) - (this.range - this.max);
-
-        // const x = Math.round(((valueX - this.min) / this.range) * this.width);
-        // const y = Math.round(((valueY - this.min) / this.range) * this.height);
+        this.draw(event);
       }
     },
 
@@ -147,12 +132,12 @@ export default {
     this.canvasOffsets = getElementOffsets(this.canvas);
     this.ctx = this.canvas.getContext('2d');
 
-    this.ctx.translate(this.width / 2, this.height / 2);
+    this.ctx.translate(this.halfSize, this.halfSize);
 
     this.drawGrid();
     this.drawAxes();
     this.axesDesignation();
-    this.drawPoint(30, -30);
+    this.drawPoint(this.vector[0] * this.halfSize, this.vector[1] * this.halfSize);
 
     this.canvas.addEventListener('mousedown', event => this.onMouseDown(event));
     document.addEventListener('mousemove', event => this.onMouseMove(event));
@@ -167,5 +152,5 @@ export default {
 </script>
 
 <template>
-  <canvas ref="vec2Picker" class="vec2-picker" :width="width" :height="height"></canvas>
+  <canvas ref="vec2Picker" class="vec2-picker" :width="size" :height="size"></canvas>
 </template>

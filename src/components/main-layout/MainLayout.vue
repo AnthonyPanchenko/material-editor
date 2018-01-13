6b16f472133b51f9977e8e8185d13db54f047791
Anton Panchenko <nixegend@gmail.com>
2018-01-13 11:15:54 +0200

    remove set interval

--- src/components/main-layout/MainLayout.vue
+++ src/components/main-layout/MainLayout.vue
@@ -1,40 +1,32 @@
 <script>
 import Notifications from '../../common/components/notifications/Notifications.vue';
 
-function s4() {
-  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-};
-
-function guid() {
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-};
-
-const getNotifications = () => ([
+const getNotifications = [
   {
-    id: guid(),
+    id: 'wte235tet',
     status: 'success',
     title: 'This is SUCCESS',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!'
   },
   {
-    id: guid(),
+    id: 'sf76567sgtet',
     status: 'warning',
     title: 'This is WARNING',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!'
   },
   {
-    id: guid(),
+    id: 'df34589g66sujrt',
     status: 'info',
     title: 'This is INFO',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!'
   },
   {
-    id: guid(),
+    id: 'qwt67457uirtyi',
     status: 'danger',
     title: 'This is DANGER',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!'
   }
-]);
+];
 
 export default {
   name: 'MainLayout',
@@ -43,21 +35,15 @@ export default {
   },
   data() {
     return {
-      notifications: getNotifications(),
+      notifications: getNotifications,
     };
   },
   methods: {
-    increase() {
-      setInterval(() => {
-        this.notifications = [getNotifications()[Math.floor(Math.random() * 4)], ...this.notifications];
-      }, 3000);
-    },
     onRemoveNotification(notificationId) {
       this.notifications = this.notifications.filter(item => item.id !== notificationId);
     }
   },
   mounted() {
-    this.increase();
   }
 }
 </script>

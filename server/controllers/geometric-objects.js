91b181f59b710bf1f63ac5a4df5d8cb0bef500b5
Anton Panchenko <nixegend@gmail.com>
2018-03-06 01:49:35 +0200

    add controllers

--- server/controllers/geometric-objects.js
+++ server/controllers/geometric-objects.js
@@ -6,9 +6,17 @@ exports.getObjects = (req, res) => {
   });
 }
 
-exports.loginWithToken = function (req, res, next) {
-  res.status(200).json({
-    token: 'JWT',
-    user: req.user
-  });
+exports.get = async function (req, res, next) {
+  const result = await GeometricObjects.find({ _id: req.params.id }).exec();
+  return res.status(200).json(result);
 };
+
+exports.update = async function (req, res, next) {
+  await GeometricObjects.update({ _id: req.params.companyId }, req.body).exec();
+  return res.status(200).send();
+}
+
+exports.remove = async function (req, res, next) {
+  await GeometricObjects.remove({ _id: req.params.companyId }).exec();
+  return res.status(200).send();
+}

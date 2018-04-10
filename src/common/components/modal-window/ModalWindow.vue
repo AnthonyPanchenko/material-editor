11aefb52ea3d8316f4cdec2ec6e5e543df70e2a4
Panchenko <anton.panchenko@sigmaukraine.com>
2018-04-10 20:04:58 +0300

    md

--- src/common/components/modal-window/ModalWindow.vue
+++ src/common/components/modal-window/ModalWindow.vue
@@ -12,14 +12,42 @@ export default {
     isMaximized: { type: Boolean, default: false },
     сloseByOverlayClick: { type: Boolean, default: true },
     onOverlayClose: { type: Function, default: noop },
-    position: { type: String, default: 'top-center' }
+    position: { type: String, default: 'center-center' }
+  },
+  data() {
+    return {
+      isMouseDown: false,
+      width: 1024,
+      height: 500,
+      offsetLeft: 0,
+      offsetTop: 0
+    };
   },
   methods: {
     onCloseModalWindow() {
       if (this.сloseByOverlayClick) {
         this.onOverlayClose();
       }
+    },
+    onMouseDown(event) {
+      console.log(event);
+    },
+    onMouseMove(event) {
+      if (this.isMouseDown) {
+        console.log(event);
+      }
+    },
+    onMouseUp() {
+      this.isMouseDown = false;
     }
+  },
+  mounted() {
+    document.addEventListener('mousemove', this.onMouseMove);
+    document.addEventListener('mouseup', this.onMouseUp);
+  },
+  beforeDestroy() {
+    document.removeEventListener('mousemove', this.onMouseMove);
+    document.removeEventListener('mouseup', this.onMouseUp);
   }
 };
 </script>
@@ -28,22 +56,22 @@ export default {
   <transition name="fade">
     <div v-if="isOpen" :class="['modal-window', { 'is-overlay': isOverlay, 'is-maximized': isMaximized }, position]" @click.self="onCloseModalWindow">
       <div v-if="isResizable" class="container">
-        <span class="top-left-corner-grab" />
-        <span class="top-right-corner-grab" />
-        <span class="top-border-grab" />
-        <span class="left-border-grab" />
-        <div v-if="isDraggable" class="header">
+        <span class="top-left-corner-grab" @mousedown="onMouseDown" />
+        <span class="top-right-corner-grab" @mousedown="onMouseDown" />
+        <span class="top-border-grab" @mousedown="onMouseDown" />
+        <span class="left-border-grab" @mousedown="onMouseDown" />
+        <div v-if="isDraggable" class="header" @mousedown="onMouseDown">
           <slot name="header">Header</slot>
         </div>
         <slot>Content here</slot>
-        <span class="bottom-left-corner-grab" />
-        <span class="right-border-grab" />
-        <span class="bottom-border-grab" />
-        <span class="bottom-right-corner-grab" />
+        <span class="bottom-left-corner-grab" @mousedown="onMouseDown" />
+        <span class="right-border-grab" @mousedown="onMouseDown" />
+        <span class="bottom-border-grab" @mousedown="onMouseDown" />
+        <span class="bottom-right-corner-grab" @mousedown="onMouseDown" />
       </div>
 
       <div v-else class="container">
-        <div v-if="isDraggable" class="header">
+        <div v-if="isDraggable" class="header" @mousedown="onMouseDown">
           <slot name="header">Header</slot>
         </div>
         <slot>Content here</slot>
